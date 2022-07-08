50477a936145720f6390c8107fc0cb4b6bbe5197
Diego Echalar Penailillo <diegoechalarp@hotmail.com>
2022-07-08 17:44:33 -0400

    Hook useEffect

---
+++ src/components/PruebasComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const PruebasComponent = () => {
+  return (
+    <div>
+        <h1>
+              El efecto - Hook useEffect
+        </h1>
+    </div>
+  )
+}
